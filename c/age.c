8811703e1e074c22bd0997f941361cf7aab66250
hitee8239 <laby8239@gmail.com>
2023-03-10 01:42:15 +0000

    versionPatch

--- c/age.c
+++ c/age.c
@@ -1,10 +1,10 @@
-#include "stdio.h"
+#include <stdio.h>
 
-int main(){
+int main() {
   int i;
-  printf("input your age : ");
-  scanf("%d", &i)
-  printf("Your age is %d \n",i);
+  printf("Input your age : ");
+  scanf("%d", &i);
+  printf("Your age is %d \n", i);
 
-  return 0 ;
+  return 0;
 }
