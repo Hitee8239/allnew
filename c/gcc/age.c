d501c79101387510d25c3ecaa3f6add2a39ab4f8
Hitee8239 <laby8239@gmail.com>
2023-03-16 15:28:56 +0900

    calc

--- c/gcc/age.c
+++ c/gcc/age.c
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-#include "stdio.h"
-
-int main(){
-  int i;
-  printf("input your age : ");
-  scanf("%d", &i)
-  printf("Your age is %d \n",i);
-
-  return 0 ;
-=======
 #include <stdio.h>
+#include "myheader.h"
 
-int main() {
-  int i;
-  printf("Input your age : ");
-  scanf("%d", &i);
-  printf("Your age is %d \n", i);
-
-  return 0;
->>>>>>> 92fc30acd9961c19fee56cee4eeb9e4153d1846b
+void main() {
+  printf("My age is %d \n", AGE );
 }
