9654e70e41b24a9b849afc1ea111915b7c021016
Hitee8239 <112450135+Hitee8239@users.noreply.github.com>
2023-03-24 14:47:24 +0900

    db
    db

---
+++ db/testdb_stinfo-stgrade_query.sql
@@ -0,0 +1,18 @@
+use testdb;
+
+SELECT NAME, DEPT from st_info ;
+SELECT NAME, DEPT from st_info WHERE ST_ID=202301;
+
+SELECT NAME, DEPT from st_info where DEPT='Game';
+
+select Linux from st_grade WHERE ST_ID=202301;
+
+#name linux DB
+SELECT st_info.name, st_grade.Linux, st_grade.DB, st_info.DEPT
+from st_info, st_grade
+where st_info.ST_ID=202301 and st_grade.ST_ID=202301;
+
+UPDATE st_grade set Linux=90 WHERE ST_ID=202301;
+
+SELECT st_info.NAME, st_grade.Linux, st_grade.DB, st_info.DEPT from st_info , st_grade 
+WHERE  st_info.ST_ID=202301 and st_grade.ST_ID=202301;
