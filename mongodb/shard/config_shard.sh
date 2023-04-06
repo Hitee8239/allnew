2bcfd23430513f9d5ea7746e3057581e2e132ae2
hitee8239 <laby8239@gmail.com>
2023-04-06 11:08:32 +0900

    shard_macro

--- mongodb/shard/config_shard.sh
+++ mongodb/shard/config_shard.sh
@@ -3,8 +3,11 @@
 # default mongodb deamon stop.
 systemctl stop mongod
 
-#shop shard shell
+#stop shard shell
 ./stop_shard.sh
+
+export IP_TEMP=$(ip addr | grep enp0s3 | grep inet | cut -d " " -f6 | cut -d "/" -f1 )
+echo $IP_TEMP
 
 # remove data directory
 if [-d data ]; then
@@ -18,6 +21,7 @@ touch /shard/data/logs/configsvr.log
 
 mongod --config /shard/mongodConfig.conf &
 sleep 3s
+mongo $IP_TEMP:27019 < rs.init
 
 # router Server
 touch /shard/data/logs/mongorouter.log
@@ -25,12 +29,14 @@ touch /shard/data/logs/mongorouter.log
 mongos --config /shard/mongodRouter.conf &
 sleep 3s
 
+
 # Shard1 Server
 mkdir -pv /shard/data/shard1db
 touch /shard/data/logs/shard1.log
 
 mongod --config /shard/mongodShard1.conf &
 sleep 2s
+mongo $IP_TEMP:27021 < rs.init
 
 # Shard2 Server 
 mkdir -pv /shard/data/shard2db
@@ -38,11 +44,13 @@ touch /shard/data/logs/shard2.log
 
 mongod --config /shard/mongodShard2.conf &
 sleep 2s
+mongo $IP_TEMP:27022 < rs.init
 
 # process status
 ps -ef | grep mongo
 sleep 2s 
 
+mongo $IP_TEMP:27017 < rs.addShard
+
 #netstatus
 netstat -ntlp
-
